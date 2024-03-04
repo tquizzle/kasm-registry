b56167b05fbcbf2768f0033534fda9f2b07a2ea8
Travis Quinnelly <tquinnelly@gmail.com>
2024-03-04 13:13:58 -0600

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
-    icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    name: 'TQ Technologies',
+    description: 'TQ\'s un-official store for Kasm-supported workspaces.',
+    icon: '/img/tq.png',
+    listUrl: 'https://github.com/tquizzle/kasm-registry/',
+    contactUrl: 'https://github.com/tquizzle/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
